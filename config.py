485b3625fe84531c6377be3cc90dea5fbcab02ce
SimonSi440 <sabianisimon@gmail.com>
2025-06-10 19:52:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 
-BOT_TOKEN = 'YOUR_BOT_TOKEN'  # Можно оставить как заглушку, если будем использовать переменные окружения
-CHANNEL_ID = '@yourchannel'  # Или числовой ID (-100...)
-GOOGLE_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1aB8XbFgZ7wUvRqJ6Qj9nDpVWxNfKcA7sYrOzZ/edit?usp=sharing'
+BOT_TOKEN = '7402291623:AAFo_br8upbId8VKm_MUwAAM7LRH3aixQ0E'  # Можно оставить как заглушку, если будем использовать переменные окружения
+CHANNEL_ID = '-1002875911388'  # Или числовой ID (-100...)
+GOOGLE_SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ7Q_C5Je2kcvPjcFdWb4x3_Sx6yR9lVAFqPGQ_OkgNMJYirk8HQZoi3-uyYcH6m7VmNgDziznnxzKb/pub?gid=0&single=true&output=csv'
